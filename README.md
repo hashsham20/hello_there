a293b3cb7d2259f650d849c59dac7ca31470e941
hashsham20 <hashshamullah20@gmail.com>
2023-05-10 20:35:46 +0200

    Changes adhering to DRY principles made.

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ A simple Flutter app that displays the text "Hello there" in the center of the s
 ## Bonus Feature: Display RGB Values
 
 This app also displays the RGB values of the current background color below the "Hello there" text. Each time the background color changes, the RGB values are updated accordingly.
+
+## Badge indiicating use of Solid Lints
+
+[![style: solid](https://img.shields.io/badge/style-solid-orange)](https://pub.dev/packages/solid_lints)
+
+## Changes adhering to the DRY Principle
+
+Combined the MyHomePagePublic and MyHomePageState classes into a single StatefulWidget.
+Used an anonymous function for the onTap event to directly update the state
--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,70 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 
+/// Entry point of the application.
 void main() {
   runApp(const MyApp());
 }
 
+/// Root widget of the application.
 class MyApp extends StatelessWidget {
+  /// This constructor allows key to be optional and is thus marked as constant
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Hello There App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePagePublic(),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePagePublic extends StatefulWidget {
-  const MyHomePagePublic({Key? key}) : super(key: key);
+/// Home page of the application.
+class MyHomePage extends StatefulWidget {
+  /// Combined the MyHomePagePublic and _MyHomePageState classes
+  /// into a single StatefulWidget
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePagePublic> {
+/// State of the [MyHomePage] widget.
+class _MyHomePageState extends State<MyHomePage> {
   Color _backgroundColor = Colors.white;
-  String _colorText = 'RGB(255, 255, 255)'; // Add a new variable for color text
-
-  void _changeBackgroundColor() {
-    setState(() {
-      final r = Random().nextInt(256);
-      final g = Random().nextInt(256);
-      final b = Random().nextInt(256);
-      _backgroundColor = Color.fromRGBO(r, g, b, 1);
-      _colorText =
-          'RGB($r, $g, $b)'; // Set the color text, will be used as a Bonus feature
-    });
-  }
+  String _colorText = 'RGB(255, 255, 255)';
+  // Add a new variable for color text
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: InkWell(
-        onTap: _changeBackgroundColor,
-        child: Container(
+        //Used a function for the onTap event to directly update the state
+        onTap: () {
+          setState(() {
+            final r = Random().nextInt(256);
+            final g = Random().nextInt(256);
+            final b = Random().nextInt(256);
+            _backgroundColor = Color.fromRGBO(r, g, b, 1);
+            _colorText = 'RGB($r, $g, $b)';
+            // Set the color text, will be used as a Bonus feature
+          });
+        },
+        child: ColoredBox(
           color: _backgroundColor,
           child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Text(
-                  'Hello there',
-                  style: TextStyle(fontSize: 24),
-                ),
-                const SizedBox(height: 10), // Add spacing between the two texts
+                const Text('Hello there', style: TextStyle(fontSize: 24)),
+                const SizedBox(height: 10),
                 Text(
-                  // Display the color text (Bonus Feature)
                   _colorText,
                   style: const TextStyle(fontSize: 20),
+                  // Display the color text (Bonus Feature)
                 ),
               ],
             ),
